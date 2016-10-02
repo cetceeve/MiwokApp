fdd087cf2fb99c1ec5839a2a58d1b4af42138e7c
Fabi <fzeiher@gmail.com>
2016-10-02 14:25:32 +0200

    feat: switched Numbers Array for ArrayList log mess. don't work (as always)

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
@@ -11,19 +13,20 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        String[] words = new String[10];
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
+        ArrayList<String> words = new ArrayList<String>();
+        words.add(0, "one");
+        words.add(1, "two");
+        words.add(2, "three");
+        words.add(3, "four");
+        words.add(4, "five");
+        words.add(5, "six");
+        words.add(6, "seven");
+        words.add(7, "eight");
+        words.add(8, "nine");
+        words.add(9, "ten");
 
-        Log.v("NumbersActivity", "Word ad index 0: " + words[0]);
-        Log.v("NumbersAcitivty", "Word at index 7: " + words[7]);
-        Log.v("NumbersActivity", "Word at index 9: " + words[9]);
+        Log.v("NumbersActivity", "Word ad index 0: " + words.get(0));
+        Log.v("NumbersAcitivty", "Word at index 7: " + words.get(7));
+        Log.v("NumbersActivity", "Word at index 9: " + words.get(9));
     }
 }
