6b3ae1837ddcbcce94b9449f46c3f20da32c748e
Fabian Zeiher <fzeiher@gmail.com>
2016-10-03 12:38:58 +0200

    refactor: ArrayList contains new Word object instead of strings (Attention: Adapter not fixed!!!)

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -15,17 +15,17 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
         //create ArrayList to display the english numbers
-        ArrayList<String> words = new ArrayList<>();
-        words.add(0, "one");
-        words.add(1, "two");
-        words.add(2, "three");
-        words.add(3, "four");
-        words.add(4, "five");
-        words.add(5, "six");
-        words.add(6, "seven");
-        words.add(7, "eight");
-        words.add(8, "nine");
-        words.add(9, "ten");
+        ArrayList<Word> words = new ArrayList<>();
+        words.add(new Word("one", "lutti"));
+        words.add(new Word("two", "otiiko"));
+        words.add(new Word("three", "tolookosu"));
+        words.add(new Word("four", "oyyisa"));
+        words.add(new Word("five", "massokka"));
+        words.add(new Word("six", "temmokka"));
+        words.add(new Word("seven", "kenekaku"));
+        words.add(new Word("eight", "kawinta"));
+        words.add(new Word("nine", "wo'e"));
+        words.add(new Word("ten", "na'aacha"));
 
         //create ArrayAdapter for the words ArrayList
         ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this,R.layout.list_item, words);
