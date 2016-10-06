7229adbd2cfe9b479e5d2b0f71eb97bd1c76a9cc
Fabi <fzeiher@gmail.com>
2016-10-06 13:13:43 +0200

    feat: add memory release for MediaPlayer refactor: MediaPlayer is now global Variable

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -11,6 +11,16 @@ import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
+    //global MediaPlayer Variable
+    private MediaPlayer mMediaPlayer;
+    //create OnCompletionListener object that calls the releaseMediaPlayer Method
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mMediaPlayer) {
+            releaseMediaPlayer();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,11 +54,32 @@ public class FamilyActivity extends AppCompatActivity {
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                     //variable of type word is created using the position in words array provided by adapter via onItemClick method
                     Word word = words.get(position);
+                    //release MediaPlayer resources to save memory
+                    releaseMediaPlayer();
                     //media player is created getting the correct id from the word object
-                    MediaPlayer mediaPlayer = MediaPlayer.create(FamilyActivity.this, word.getAudioResourceId());
-                    mediaPlayer.start();
+                    mMediaPlayer = MediaPlayer.create(FamilyActivity.this, word.getAudioResourceId());
+                    mMediaPlayer.start();
+                    //Listener triggers releaseMediaPlayer Method to release memory
+                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
                 }
             });
+        }
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
         }
     }
 }
